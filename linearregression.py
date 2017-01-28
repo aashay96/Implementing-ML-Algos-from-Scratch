e408f68f6722c1fe9e5f8d056a0b5c64f14a4be8
aashay96 <aashay96@gmail.com>
2017-01-28 10:21:02 +0530

    R-squared mean

--- linearregression.py
+++ linearregression.py
@@ -14,6 +14,15 @@ def best_fit_slope_intercept(xs,ys):
     b = mean(ys) - m*mean(xs)
     return m,b
 
+def squared_error(ys_orig,ys_line):
+    return sum((ys_orig-ys_line)**2)
+
+def coefficient_of_determination(ys_orig,ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig,ys_line)
+    squared_error_y_mean = squared_error(ys_orig,y_mean_line)
+    return 1-(squared_error_regr/squared_error_y_mean)
+
 m,b= best_fit_slope_intercept(xs,ys)
 
 regression_line = [(m*x+b) for x in xs]
@@ -21,9 +30,9 @@ regression_line = [(m*x+b) for x in xs]
 predict_x = 8
 predict_y = predict_x*m +b
 print(predict_y)
-plt.scatter(xs,ys)
-plt.plot(xs,regression_line)
-plt.show()
+print coefficient_of_determination(ys,regression_line)
+
+
 
 print(m)
 print(b)
